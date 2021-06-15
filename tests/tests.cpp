e2787ceb68945b1c0f2957f16a5975b9927a701d
erikvalkering <erikvalkering@gmail.com>
2021-06-15 23:57:12 +0200

    Fixed failing unit test test_dependencies(): arguments will now also be considered in is-changed check

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,13 +6,17 @@ using namespace std;
 
 namespace pigro {
 
-constexpr auto lazy = [](auto f) {
-    using result_t = decltype(f());
+constexpr auto lazy = [](auto f, auto dep) {
+    using result_t = decltype(f(dep()));
+    using dep_result_t = decltype(dep());
 
     auto cache = std::optional<result_t>{};
+    auto dep_cache = std::optional<dep_result_t>{};
     return [=]() mutable {
-        if (!cache) {
-            cache = f();
+        const auto arg = dep();
+        if (!cache || arg != dep_cache) {
+            dep_cache = arg;
+            cache = f(*dep_cache);
         }
 
         return *cache;
