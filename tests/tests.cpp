087deccc092944eab8e42286e6839aa6b91aedbd
erikvalkering <erikvalkering@gmail.com>
2021-06-15 22:50:13 +0200

    Added failing and non-compiling test_dependencies() unit test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -40,8 +40,43 @@ auto test_cached() {
     assert(counter == 1);
 }
 
+auto test_dependencies() {
+    cout << "test_dependencies" << endl;
+
+    auto bar_counter = 0;
+    auto bar_result = 40;
+    const auto bar = [&] {
+        ++bar_counter;
+        return bar_result;
+    };
+
+    auto foo_counter = 0;
+    auto foo = lazy([&](auto bar) {
+        ++foo_counter;
+        return bar + 2;
+    },
+      bar);
+
+    assert(foo_counter == 0);
+    assert(bar_counter == 0);
+
+    assert(foo() == 42);
+    assert(foo_counter == 1);
+    assert(bar_counter == 1);
+
+    assert(foo() == 42);
+    assert(foo_counter == 1);
+    assert(bar_counter == 2);
+
+    ++bar_result;
+    assert(foo() == 43);
+    assert(foo_counter == 2);
+    assert(bar_counter == 3);
+}
+
 auto test_pigro() {
     test_cached();
+    test_dependencies();
 }
 
 } // namespace pigro::tests
