a8faa80fb99cd3fbda651f26ecc802b5b324eccc
erikvalkering <erikvalkering@gmail.com>
2021-06-19 22:05:09 +0200

    Simplified implementation of recursive_overload by expressing it in terms of recursive+overload

--- src/pigro/recursive_overload.h
+++ src/pigro/recursive_overload.h
@@ -1,42 +1,14 @@
 #pragma once
 
+#include "overload.h"
+#include "recursive.h"
+
 namespace pigro {
 
-namespace detail {
+template<typename... Fs>
+using recursive_overload = recursive<overload<Fs...>>;
 
-template<typename... F>
-struct recursive_overload : F... {
-private:
-    using F::operator()...;
-
-protected:
-    template<typename... Args>
-    decltype(auto) call(Args &&...args) const {
-        return (*this)(forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    decltype(auto) call(Args &&...args) {
-        return (*this)(forward<Args>(args)...);
-    }
-};
-
-} // namespace detail
-
-template<typename... F>
-struct recursive_overload : detail::recursive_overload<F...> {
-    template<typename... Args>
-    decltype(auto) operator()(Args &&...args) const {
-        return this->call(*this, forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    decltype(auto) operator()(Args &&...args) {
-        return this->call(*this, forward<Args>(args)...);
-    }
-};
-
-template<typename... F>
-recursive_overload(F...) -> recursive_overload<F...>;
+template<typename... Fs>
+recursive_overload(Fs...) -> recursive_overload<Fs...>;
 
 } // namespace pigro
