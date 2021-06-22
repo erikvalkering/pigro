56acb1081d71568579fc4923c27e2238d4ba388c
erikvalkering <erikvalkering@gmail.com>
2021-06-22 22:49:27 +0200

    Added unit test to check for multi-level lazy dependency support

--- tests/lazy.test.cpp
+++ tests/lazy.test.cpp
@@ -62,4 +62,55 @@ auto test_dependencies = [] {
     return 0;
 }();
 
+auto test_lazy_dependencies = [] {
+    cout << "test_lazy_dependencies" << endl;
+
+    auto baz_counter = 0;
+    auto baz_result = 0;
+    const auto baz = [&] {
+        ++baz_counter;
+        return baz_result;
+    };
+
+    auto bar_counter = 0;
+    auto bar = lazy([&](auto baz) {
+        ++bar_counter;
+        return baz + 2;
+    },
+      baz);
+
+    auto foo_counter = 0;
+    auto foo = lazy([&](auto bar) {
+        ++foo_counter;
+        return bar + 40;
+    },
+      bar);
+
+    assert(foo_counter == 0);
+    assert(bar_counter == 0);
+    assert(baz_counter == 0);
+    assert(foo() == 42);
+    assert(foo_counter == 1);
+    assert(bar_counter == 1);
+    assert(baz_counter == 1);
+
+    assert(foo() == 42);
+    assert(foo_counter == 1);
+    assert(bar_counter == 1);
+    assert(baz_counter == 2);
+
+    ++baz_result;
+    assert(foo() == 43);
+    assert(foo_counter == 2);
+    assert(bar_counter == 2);
+    assert(baz_counter == 3);
+
+    assert(foo() == 43);
+    assert(foo_counter == 2);
+    assert(bar_counter == 2);
+    assert(baz_counter == 4);
+
+    return 0;
+}();
+
 } // namespace pigro::tests
