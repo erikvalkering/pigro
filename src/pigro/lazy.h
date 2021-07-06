83a1edd45b0092319096caf9b68562230125ba9e
erikvalkering <erikvalkering@gmail.com>
2021-07-06 19:54:53 +0200

    Fixed failing test_lazy_dependencies test by avoid any comparisons with the dependencies
    This is quite a big refactoring, but allows for a much more flexible design by introducing a `lazy_function` concept, which returns a LazyResult<T>. This way, we can create many specialized flavors of lazy functions, that are for example optimized for minimal memory footprint.

--- src/pigro/lazy.h
+++ src/pigro/lazy.h
@@ -1,24 +1,83 @@
 #pragma once
 
+#include "overload.h"
+#include "recursive.h"
+
+#include <concepts>
+#include <cstddef>
 #include <optional>
+#include <utility>
 
 namespace pigro {
 
-auto lazy(auto f, auto dep) {
-    using result_t = decltype(f(dep()));
-    using dep_result_t = decltype(dep());
+template<typename T>
+struct LazyResult {
+    T value;
+    bool is_changed;
+};
+
+template<typename T>
+LazyResult(const T &, bool) -> LazyResult<const T &>;
+LazyResult(int &&, bool)->LazyResult<int>;
+
+template<typename F>
+concept lazy_function = requires(F f) {
+    f(nullptr);
+};
+
+constexpr auto unwrap(lazy_function auto lazy_f) {
+    return recursive{
+        overload{
+          [=](auto &, std::nullptr_t) mutable {
+              return lazy_f(nullptr);
+          },
+          [](auto &self) {
+              return self(nullptr).value;
+          },
+        }
+    };
+}
+
+constexpr auto lazy(auto f, lazy_function auto dep) {
+    using result_t = decltype(f(dep(nullptr).value));
 
     auto cache = std::optional<result_t>{};
-    auto dep_cache = std::optional<dep_result_t>{};
-    return [=]() mutable {
-        const auto arg = dep();
-        if (!cache || arg != dep_cache) {
-            dep_cache = arg;
-            cache = f(*dep_cache);
+    return unwrap([=](std::nullptr_t) mutable {
+        const auto arg = dep(nullptr);
+
+        auto changed = !cache || arg.is_changed;
+        if (changed) {
+            const auto value = arg.value;
+            const auto result = f(value);
+
+            changed = cache != result;
+            cache = std::move(result);
         }
 
-        return *cache;
+        return LazyResult{
+            *cache,
+            changed,
+        };
+    });
+}
+
+constexpr auto lazy_value(auto value, auto changed) {
+    return [=](std::nullptr_t) {
+        return LazyResult{
+            value,
+            changed,
+        };
     };
+};
+
+constexpr auto lazy_always_reevaluate(std::invocable auto f) {
+    return lazy(
+      [=](auto) mutable { return f(); },
+      lazy_value(0, std::true_type{}));
+};
+
+auto lazy(auto f, auto dep) {
+    return lazy(f, lazy_always_reevaluate(dep));
 }
 
 } // namespace pigro
