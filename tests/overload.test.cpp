f0c3e6095089ef430410583b7b8be3158fc796a9
erikvalkering <erikvalkering@gmail.com>
2021-07-06 09:10:05 +0200

    Converted overload tests to use [Boost::Ext].UT

--- tests/overload.test.cpp
+++ tests/overload.test.cpp
@@ -1,68 +1,63 @@
 #include "../src/pigro/overload.h"
 
-#include <cassert>
-#include <iostream>
+#define BOOST_UT_DISABLE_MODULE
+#include <boost/ut.hpp>
 
+using namespace boost::ut;
 using namespace std;
 
 namespace pigro::tests {
 
-auto test_overload = [] {
-    cout << "test_overload" << endl;
+suite overload_tests = [] {
+    "test_overload"_test = [] {
+        const auto f = overload{
+            [](int) { return "int"s; },
+            [](double) { return "double"s; },
+            [](auto) { return "auto"s; },
+        };
 
-    const auto f = overload{
-        [](int) { return "int"s; },
-        [](double) { return "double"s; },
-        [](auto) { return "auto"s; },
+        expect(f(0) == "int"s);
+        expect(f(0.0) == "double"s);
+        expect(f(true) == "auto"s);
     };
 
-    assert(f(0) == "int");
-    assert(f(0.0) == "double");
-    assert(f(true) == "auto");
+    "test_ebo_msvc_workaround"_test = [] {
+        auto r1 = overload{
+            [] {},
+            [] {},
+        };
+        auto r2 = overload{
+            [] {},
+            [x = 1] { return x; },
+        };
+        auto r3 = overload{
+            [x = 'a'] { return x; },
+            [] {},
+        };
+        auto r4 = overload{
+            [] {},
+            [x = 1] { return x; },
+            [] {},
+            [x = 1] { return x; },
+        };
+        auto r5 = overload{
+            [x = 1] { return x; },
+            [] {},
+            [x = 1] { return x; },
+            [] {},
+        };
 
-    return 0;
-}();
-
-auto test_ebo_msvc_workaround = [] {
-    cout << "test_ebo_msvc_workaround" << endl;
-
-    auto r1 = overload{
-        [] {},
-        [] {},
+        static_assert(is_empty_v<decltype(r1)>);
+        static_assert(sizeof(r1) == sizeof(char));
+        static_assert(!is_empty_v<decltype(r2)>);
+        static_assert(sizeof(r2) == sizeof(int));
+        static_assert(!is_empty_v<decltype(r3)>);
+        static_assert(sizeof(r3) == sizeof(char));
+        static_assert(!is_empty_v<decltype(r4)>);
+        static_assert(sizeof(r4) == 2 * sizeof(int));
+        static_assert(!is_empty_v<decltype(r5)>);
+        static_assert(sizeof(r5) == 2 * sizeof(int));
     };
-    auto r2 = overload{
-        [] {},
-        [x = 1] { return x; },
-    };
-    auto r3 = overload{
-        [x = 'a'] { return x; },
-        [] {},
-    };
-    auto r4 = overload{
-        [] {},
-        [x = 1] { return x; },
-        [] {},
-        [x = 1] { return x; },
-    };
-    auto r5 = overload{
-        [x = 1] { return x; },
-        [] {},
-        [x = 1] { return x; },
-        [] {},
-    };
-
-    static_assert(is_empty_v<decltype(r1)>);
-    static_assert(sizeof(r1) == sizeof(char));
-    static_assert(!is_empty_v<decltype(r2)>);
-    static_assert(sizeof(r2) == sizeof(int));
-    static_assert(!is_empty_v<decltype(r3)>);
-    static_assert(sizeof(r3) == sizeof(char));
-    static_assert(!is_empty_v<decltype(r4)>);
-    static_assert(sizeof(r4) == 2 * sizeof(int));
-    static_assert(!is_empty_v<decltype(r5)>);
-    static_assert(sizeof(r5) == 2 * sizeof(int));
-
-    return 0;
-}();
+};
 
 } // namespace pigro::tests
