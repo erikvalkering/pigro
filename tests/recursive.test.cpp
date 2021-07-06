a79ecbed2563f92ea7f78dc762b0f61adffe1e70
erikvalkering <erikvalkering@gmail.com>
2021-07-06 09:15:19 +0200

    Converted recursive tests to use [Boost::Ext].UT

--- tests/recursive.test.cpp
+++ tests/recursive.test.cpp
@@ -1,32 +1,31 @@
 #include "../src/pigro/recursive.h"
 
-#include <cassert>
-#include <iostream>
+#define BOOST_UT_DISABLE_MODULE
+#include <boost/ut.hpp>
 
+using namespace boost::ut;
 using namespace std;
 
 namespace pigro::tests {
 
-auto test_recursive = [] {
-    cout << "test_recursive" << endl;
+suite recursive_tests = [] {
+    "test_recursive"_test = [] {
+        auto fibonacci = recursive{
+            [](auto self, int n) -> int {
+                if (n < 2) return 1;
+                return self(n - 2) + self(n - 1);
+            }
+        };
 
-    auto fibonacci = recursive{
-        [](auto self, int n) -> int {
-            if (n < 2) return 1;
-            return self(n - 2) + self(n - 1);
-        }
+        expect(fibonacci(0) == 1_i);
+        expect(fibonacci(1) == 1_i);
+        expect(fibonacci(2) == 2_i);
+        expect(fibonacci(3) == 3_i);
+        expect(fibonacci(4) == 5_i);
+
+        static_assert(sizeof(fibonacci) == sizeof(char));
+        static_assert(std::is_empty_v<decltype(fibonacci)>);
     };
-
-    assert(fibonacci(0) == 1);
-    assert(fibonacci(1) == 1);
-    assert(fibonacci(2) == 2);
-    assert(fibonacci(3) == 3);
-    assert(fibonacci(4) == 5);
-
-    static_assert(sizeof(fibonacci) == sizeof(char));
-    static_assert(std::is_empty_v<decltype(fibonacci)>);
-
-    return 0;
-}();
+};
 
 } // namespace pigro::tests
