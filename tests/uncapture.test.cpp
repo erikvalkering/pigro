52c61ee391e4daee4890e16db8da73a0b7378559
erikvalkering <erikvalkering@gmail.com>
2021-07-13 01:47:00 +0200

    Extended unit tests for uncapture utility

--- tests/uncapture.test.cpp
+++ tests/uncapture.test.cpp
@@ -30,13 +30,18 @@ suite uncapture_tests = [] {
 
     "extra_parameters"_test = [] {
         auto f = uncaptured(empty) >> [](int, Empty) { return 0; };
-
         expect(f(0) == 0_i);
+        expect(constant<std::is_empty_v<decltype(f)>>);
     };
 
     "variadic"_test = [] {
-        auto f = uncaptured(empty, empty, empty) >> [](auto...) {};
-        expect(constant<std::is_empty_v<decltype(f)>>);
+        auto f1 = uncaptured(empty, empty, empty) >> [](auto...) { return 0; };
+        expect(f1(0) == 0_i);
+        expect(constant<std::is_empty_v<decltype(f1)>>);
+
+        auto f2 = uncaptured(1, 10, 100) >> [](int a, int b, int c) { return c - b - a; };
+        expect(f2(0) == 100 - 10 - 1);
+        expect(constant<!std::is_empty_v<decltype(f2)>>);
     };
 };
 
