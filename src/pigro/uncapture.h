d753033d8fac9dc7e3a6dcda242c422f46f2b494
erikvalkering <erikvalkering@gmail.com>
2021-07-13 00:24:03 +0200

    Added uncapture utility

---
+++ src/pigro/uncapture.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <type_traits>
+
+namespace pigro::detail {
+
+template<typename T>
+struct empty_object {
+    struct T1 {
+        char c;
+    };
+    struct T2 : T {
+        char c;
+    };
+
+    union Storage {
+        constexpr Storage() : t1{} {}
+        T1 t1;
+        T2 t2;
+    };
+
+    static T get() {
+        Storage storage{};
+
+        const auto *c = &storage.t2.c;
+
+        auto t2 = reinterpret_cast<const T2 *>(c);
+        auto t = static_cast<const T *>(t2);
+
+        return *t;
+    }
+};
+
+template<typename T>
+concept empty = std::is_empty_v<T>;
+
+template<typename T>
+struct Uncaptured {
+    T value;
+
+    auto get_value() const { return value; }
+};
+
+template<empty T>
+struct Uncaptured<T> {
+    Uncaptured(T) {}
+
+    auto get_value() const { return empty_object<T>::get(); }
+};
+
+template<typename U, typename F>
+struct CompressedInvocable : private U
+  , F {
+    auto operator()() const {
+        return F::operator()(U::get_value());
+    }
+
+    CompressedInvocable(U u, F f) : U{ u }, F{ f } {};
+};
+
+template<typename T, typename F>
+auto operator>>(Uncaptured<T> u, F f) {
+    return CompressedInvocable<Uncaptured<T>, F>{ u, f };
+}
+
+} // namespace pigro::detail
+
+namespace pigro {
+
+auto uncaptured(auto value) {
+    return detail::Uncaptured<decltype(value)>{ value };
+}
+
+} // namespace pigro
---
+++ tests/uncapture.test.cpp
@@ -0,0 +1,43 @@
+#include "../src/pigro/uncapture.h"
+
+#define BOOST_UT_DISABLE_MODULE
+#include <boost/ut.hpp>
+
+using namespace boost::ut;
+
+namespace pigro::tests {
+
+struct Empty {
+    auto operator<=>(const Empty &) const = default;
+} empty;
+
+suite uncapture_tests = [] {
+    "uncapture"_test = [] {
+        expect(constant<std::is_empty_v<decltype(empty)>>);
+
+        auto f1 = [] { return 0; };
+        expect(f1() == 0_i);
+        expect(constant<std::is_empty_v<decltype(f1)>>);
+
+        auto f2 = [=] { return empty; };
+        expect(f2() == empty);
+        expect(constant<!std::is_empty_v<decltype(f2)>>);
+
+        auto f3 = uncaptured(empty) >> [](auto empty) { return empty; };
+        expect(f2() == empty);
+        expect(constant<std::is_empty_v<decltype(f3)>>);
+    };
+
+    "extra_parameters"_test = [] {
+        auto f = uncaptured(empty) >> [](int, Empty) { return 0; };
+
+        expect(f(0) == 0_i);
+    };
+
+    "variadic"_test = [] {
+        auto f = uncaptured(empty, empty, empty) >> [](auto...) {};
+        expect(constant<std::is_empty_v<decltype(f)>>);
+    };
+};
+
+} // namespace pigro::tests
