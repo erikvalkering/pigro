c754ed73367ca16ea614505966b50d4a0e42c951
erikvalkering <erikvalkering@gmail.com>
2021-08-29 19:07:25 +0200

    Fixed failing unit test

--- src/pigro/pack_algorithms.h
+++ src/pigro/pack_algorithms.h
@@ -11,7 +11,7 @@ struct Enumerator {
 };
 
 auto enumerate_pack(auto f, auto... pack) {
-    return [ =, t = std::tuple{ pack... } ]<size_t... idx>(std::index_sequence<idx...>) {
+    return [ =, t = std::make_tuple(pack...) ]<size_t... idx>(std::index_sequence<idx...>) {
         return f(Enumerator<idx, decltype(get<idx>(t))>{ get<idx>(t) }...);
     }
     (std::make_index_sequence<sizeof...(pack)>{});
