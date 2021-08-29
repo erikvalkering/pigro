31f5b26911af68e00a4053766fa326e25ab452fa
erikvalkering <erikvalkering@gmail.com>
2021-08-29 18:54:59 +0200

    Fixed some bugs in the unit tests for pack/tuple algorithms

--- tests/pack_algorithms.test.cpp
+++ tests/pack_algorithms.test.cpp
@@ -32,23 +32,23 @@ suite pack_algorithms_tests = [] {
             3,
             4,
             5)
-          == 25);
+          == 15);
     };
 
     "enumerate_tuple"_test = [] {
         expect(
-          enumerate_pack([](auto... items) {
+          enumerate_tuple([](auto... items) {
               return (decltype(items)::index + ...);
           },
             tuple{ 1, 2, 3, 4, 5 })
           == 10);
 
         expect(
-          enumerate_pack([](auto... items) {
+          enumerate_tuple([](auto... items) {
               return (items.value + ...);
           },
             tuple{ 1, 2, 3, 4, 5 })
-          == 25);
+          == 15);
     };
 
     "enumerate_n"_test = [] {
