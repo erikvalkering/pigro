12be127c14b6e5fdc45ae8dec2b6fa4d5504fed4
erikvalkering <erikvalkering@gmail.com>
2021-08-31 04:03:39 +0200

    Fixed implementation of recursive utility

--- src/pigro/recursive.h
+++ src/pigro/recursive.h
@@ -7,14 +7,53 @@
 namespace pigro {
 
 template<typename F>
-struct recursive : F {
-    template<typename... Args>
-    constexpr auto operator()(Args &&...args) const
-      SFINAEABLE_RETURN(F::operator()(*this, std::forward<Args>(args)...));
+struct recursive_impl;
+
+template<typename F>
+concept has_operator_call = requires {
+    {
+        F::operator()
+    };
+};
+
+template<has_operator_call F>
+struct recursive_impl<F> : F {
+    using F::operator();
 
     template<typename... Args>
-    constexpr auto operator()(Args &&...args)
-      SFINAEABLE_RETURN(F::operator()(*this, std::forward<Args>(args)...));
+    constexpr decltype(auto) operator_call(Args &&...args) {
+        return (*this)(std::forward<Args>(args)...);
+    }
+
+    template<typename... Args>
+    constexpr decltype(auto) operator_call(Args &&...args) const {
+        return (*this)(std::forward<Args>(args)...);
+    }
+};
+
+template<typename F>
+struct recursive_impl {
+    explicit recursive_impl(...) {}
+    constexpr auto operator_call(...) const;
+};
+
+template<typename F>
+struct recursive : private recursive_impl<F> {
+    static_assert(!std::is_const_v<F>);
+    static_assert(!std::is_reference_v<F>);
+    explicit recursive(F f) : recursive_impl<F>{ f } {}
+
+    using recursive_impl<F>::operator_call;
+
+    template<typename... Args>
+    constexpr decltype(auto) operator()(Args &&...args) {
+        return operator_call(*this, std::forward<Args>(args)...);
+    }
+
+    template<typename... Args>
+    constexpr decltype(auto) operator()(Args &&...args) const {
+        return operator_call(*this, std::forward<Args>(args)...);
+    }
 };
 
 template<typename F>
