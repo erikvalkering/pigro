2dc10529951a9d662434af64df32635039780ed5
erikvalkering <erikvalkering@gmail.com>
2021-08-31 03:34:05 +0200

    Added idx utility to more easily create std::integral_constant<size_t, N>

--- src/pigro/utils.h
+++ src/pigro/utils.h
@@ -4,3 +4,13 @@
     ->decltype(__VA_ARGS__) {  \
         return __VA_ARGS__;    \
     }
+
+namespace pigro {
+
+template<size_t index>
+using idx_t = std::integral_constant<size_t, index>;
+
+template<size_t index>
+constexpr auto idx = idx_t<index>{};
+
+} // namespace pigro
