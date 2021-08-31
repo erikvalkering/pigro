b5521d01c8b64d713f6014d77d19e968168ab6ac
erikvalkering <erikvalkering@gmail.com>
2021-08-31 03:36:43 +0200

    Fixed an issue in bind_front_tuple

--- src/pigro/bind_tuple.h
+++ src/pigro/bind_tuple.h
@@ -2,6 +2,7 @@
 
 #include "compressed_tuple.h"
 #include "concepts.h"
+#include "pack_algorithms.h"
 
 namespace pigro {
 
@@ -10,11 +11,14 @@ auto bind_front_tuple(auto f, concepts::tuple_like auto t) {
         overload{
           compressed_tuple_element<0>(f),
           compressed_tuple_element<1>(t),
-          [](auto &self) mutable {
+          [](auto &&self) {
               auto &&f = self(idx<0>);
               auto &&compressed_args = self(idx<1>);
 
-              return std::apply(f, compressed_args);
+              return enumerate_n<std::tuple_size_v<decltype(compressed_args)>>(
+                [=](auto... items) {
+                    return f(std::get<items.index>(compressed_args)...);
+                });
           },
         }
     };
