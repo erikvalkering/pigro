8d94e482531a57067fe3cf04d3595a3d548230b3
erikvalkering <erikvalkering@gmail.com>
2021-09-14 21:36:46 +0200

    Moved down compressed_tuple test

--- tests/compressed_tuple.test.cpp
+++ tests/compressed_tuple.test.cpp
@@ -18,47 +18,6 @@ struct Empty {
 suite compressed_tuple_tests = [] {
     auto empty = Empty{};
 
-    "basic"_test = [=] {
-        auto t1 = compressed_tuple{};
-        expect(constant<sizeof(t1) == sizeof(empty)>);
-        expect(constant<std::is_empty_v<decltype(t1)>>);
-
-        auto x = int{ 1 };
-        auto t2 = compressed_tuple{ x };
-
-        expect(std::get<0>(t2) == x);
-        expect(constant<sizeof(t2) == sizeof(int)>);
-        expect(constant<!std::is_empty_v<decltype(t2)>>);
-
-        auto y = double{ 2.0 };
-        auto t3 = compressed_tuple{ x, y };
-
-        expect(std::get<0>(t3) == x);
-        expect(std::get<1>(t3) == y);
-        expect(constant<sizeof(t3) == sizeof(double) + sizeof(double)>);
-        expect(constant<!std::is_empty_v<decltype(t3)>>);
-
-        auto t4 = compressed_tuple{ empty };
-
-        expect(std::get<0>(t4) == empty);
-        expect(constant<sizeof(t4) == sizeof(empty)>);
-        expect(constant<std::is_empty_v<decltype(t4)>>);
-
-        auto t5 = compressed_tuple{ empty, y };
-
-        expect(std::get<0>(t5) == empty);
-        expect(std::get<1>(t5) == y);
-        expect(constant<sizeof(t5) == 0 + sizeof(double)>);
-        expect(constant<!std::is_empty_v<decltype(t5)>>);
-
-        auto t6 = compressed_tuple{ y, empty };
-
-        expect(std::get<0>(t6) == y);
-        expect(std::get<1>(t6) == empty);
-        expect(constant<sizeof(t6) == sizeof(double) + 0>);
-        expect(constant<!std::is_empty_v<decltype(t6)>>);
-    };
-
     "compressed_tuple_element_stateful"_test = [] {
         auto e1 = recursive{ compressed_tuple_element<0>(0) };
         expect(e1(idx<0>) == 0);
@@ -133,6 +92,47 @@ suite compressed_tuple_tests = [] {
         const auto g = f;
         expect(g(idx<0>) == 0_i);
         expect(g(0) == 1_i);
+    };
+
+    "basic"_test = [=] {
+        auto t1 = compressed_tuple{};
+        expect(constant<sizeof(t1) == sizeof(empty)>);
+        expect(constant<std::is_empty_v<decltype(t1)>>);
+
+        auto x = int{ 1 };
+        auto t2 = compressed_tuple{ x };
+
+        expect(std::get<0>(t2) == x);
+        expect(constant<sizeof(t2) == sizeof(int)>);
+        expect(constant<!std::is_empty_v<decltype(t2)>>);
+
+        auto y = double{ 2.0 };
+        auto t3 = compressed_tuple{ x, y };
+
+        expect(std::get<0>(t3) == x);
+        expect(std::get<1>(t3) == y);
+        expect(constant<sizeof(t3) == sizeof(double) + sizeof(double)>);
+        expect(constant<!std::is_empty_v<decltype(t3)>>);
+
+        auto t4 = compressed_tuple{ empty };
+
+        expect(std::get<0>(t4) == empty);
+        expect(constant<sizeof(t4) == sizeof(empty)>);
+        expect(constant<std::is_empty_v<decltype(t4)>>);
+
+        auto t5 = compressed_tuple{ empty, y };
+
+        expect(std::get<0>(t5) == empty);
+        expect(std::get<1>(t5) == y);
+        expect(constant<sizeof(t5) == 0 + sizeof(double)>);
+        expect(constant<!std::is_empty_v<decltype(t5)>>);
+
+        auto t6 = compressed_tuple{ y, empty };
+
+        expect(std::get<0>(t6) == y);
+        expect(std::get<1>(t6) == empty);
+        expect(constant<sizeof(t6) == sizeof(double) + 0>);
+        expect(constant<!std::is_empty_v<decltype(t6)>>);
     };
 
     "lvalue_reference"_test = [] {
