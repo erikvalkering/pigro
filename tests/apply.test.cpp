0af54dc8840a68bf6f82e420db09274e4a0eda57
erikvalkering <erikvalkering@gmail.com>
2021-09-18 13:00:02 +0200

    Added SFINAE test for pigro::apply

--- tests/apply.test.cpp
+++ tests/apply.test.cpp
@@ -1,5 +1,6 @@
 #include "../src/pigro/apply.h"
 #include "../src/pigro/compressed_tuple.h"
+#include "../src/pigro/overload.h"
 
 #define BOOST_UT_DISABLE_MODULE
 #include <boost/ut.hpp>
@@ -20,6 +21,18 @@ suite apply_tests = [] {
 
         auto t2 = pigro::compressed_tuple{ 1, 2, 3 };
         expect(pigro::apply(f, t2) == 4_i);
+    };
+
+    "SFINAE-friendliness"_test = [] {
+        auto f = [](nullptr_t) { return 1; };
+
+        auto g = overload{
+            [=](auto t) -> decltype(pigro::apply(f, t)) { return pigro::apply(f, t); },
+            [](auto t) { return 2; },
+        };
+
+        expect(g(pigro::compressed_tuple{ nullptr }) == 1_i);
+        expect(g(pigro::compressed_tuple{ 1.0 }) == 2_i);
     };
 };
 
