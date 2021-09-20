baccb3537d2f8ae2112146ff6d2b27e5bd9fe5e0
erikvalkering <erikvalkering@gmail.com>
2021-09-20 17:41:41 +0200

    Fixed pigro::concepts::tuple_like

--- src/pigro/concepts.h
+++ src/pigro/concepts.h
@@ -26,17 +26,18 @@ concept empty = std::is_empty_v<std::remove_cvref_t<T>>;
 template<std::size_t I, class T>
 concept has_tuple_element =
   requires {
-    typename std::tuple_element<I, std::remove_const_t<T>>::type;
+    typename std::tuple_element<I, T>::type;
 };
 
 template<class T>
-concept tuple_like = !std::is_reference_v<T> && requires {
-    typename std::tuple_size<T>;
-    std::same_as<decltype(std::tuple_size_v<T>), size_t>;
+concept tuple_like = requires {
+    typename std::tuple_size<std::remove_reference_t<T>>;
+    std::same_as<decltype(std::tuple_size_v<std::remove_reference_t<T>>), size_t>;
 }
 &&[]<std::size_t... I>(std::index_sequence<I...>) {
-    return (has_tuple_element<I, T> && ...);
+    return (has_tuple_element<I, std::remove_reference_t<T>> && ...);
 }
 (std::make_index_sequence<std::tuple_size_v<T>>());
+;
 
 } // namespace pigro::concepts
