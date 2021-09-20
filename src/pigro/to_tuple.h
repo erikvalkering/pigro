29e13a91fd4efdafc2e74f79d17e64b413bea634
erikvalkering <erikvalkering@gmail.com>
2021-09-20 19:22:07 +0200

    Simplified implementation of to_tuple

--- src/pigro/to_tuple.h
+++ src/pigro/to_tuple.h
@@ -4,13 +4,14 @@
 #include "concepts.h"
 
 #include <tuple>
+#include <utility>
 
 namespace pigro {
 
 auto to_tuple(concepts::tuple_like auto &&t) {
     return pigro::apply(
       [](auto &&...values) {
-          return std::tuple<decltype(values)...>{ std::forward<decltype(values)>(values)... };
+          return std::forward_as_tuple(std::forward<decltype(values)>(values)...);
       },
       std::forward<decltype(t)>(t));
 }
