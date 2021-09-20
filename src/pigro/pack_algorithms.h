21f393d27e0aa345b83353df55b07dc33631a189
erikvalkering <erikvalkering@gmail.com>
2021-09-20 17:34:57 +0200

    Simplified some template arguments

--- src/pigro/pack_algorithms.h
+++ src/pigro/pack_algorithms.h
@@ -11,8 +11,8 @@ struct Item {
     T value;
     constexpr static auto index = idx;
 };
-template<typename... Pack, typename F>
-auto enumerate_pack(F &&f, Pack &&...pack) {
+
+auto enumerate_pack(auto &&f, auto &&...pack) {
     return [&, t = std::tuple<decltype(pack)...>{ std::forward<decltype(pack)>(pack)... } ]<size_t... idx>(std::index_sequence<idx...>) mutable {
         return f(
           Item<
