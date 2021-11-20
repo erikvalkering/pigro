727062a040b25b3e40975994175530e332c449ea
erikvalkering <erikvalkering@gmail.com>
2021-11-20 16:38:09 +0100

    Fixed some SFINAE unfriendliness issues

--- src/pigro/bind_tuple.h
+++ src/pigro/bind_tuple.h
@@ -11,7 +11,8 @@
 namespace pigro {
 
 auto bind_back(auto &&f, auto &&...back_args) {
-    return [f = fwd_capture(std::forward<decltype(f)>(f)), ... back_args = fwd_capture(std::forward<decltype(back_args)>(back_args))](auto &&...front_args) mutable {
+    return [f = fwd_capture(std::forward<decltype(f)>(f)), ... back_args = fwd_capture(std::forward<decltype(back_args)>(back_args))](auto &&...front_args) mutable
+           -> decltype(std::invoke(std::forward<decltype(f)>(f), std::forward<decltype(front_args)>(front_args)..., std::forward<decltype(back_args)>(back_args)...)) {
         return std::invoke(access(f), std::forward<decltype(front_args)>(front_args)..., access(back_args)...);
     };
 }
@@ -21,7 +22,8 @@ auto bind_front_tuple(auto &&f, concepts::tuple_like auto &&t) {
         overload{
           compressed_tuple_element<0>(std::forward<decltype(f)>(f)),
           compressed_tuple_element<1>(std::forward<decltype(t)>(t)),
-          [](auto &&self, auto &&...args) {
+          [](auto &&self, auto &&...args)
+            -> decltype(pigro::apply(bind_back(f, std::forward<decltype(args)>(args)...), t)) {
               auto &&f = self(idx<0>);
               auto &&t = self(idx<1>);
 
