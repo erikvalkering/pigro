5293f1bfefbb521bf2a5e8c3ccb05dae5e484cd3
erikvalkering <erikvalkering@gmail.com>
2022-02-02 23:53:46 +0100

    Implemented a slightly better bind_back (version 3)

--- src/pigro/bind_tuple.h
+++ src/pigro/bind_tuple.h
@@ -5,17 +5,30 @@
 #include "concepts.h"
 #include "fwd_capture.h"
 #include "pack_algorithms.h"
+#include "recursive.h"
 
+#include <concepts>
 #include <functional>
 
 namespace pigro {
 
-auto bind_back(auto &&f, auto &&...back_args) {
-    return [f = FWD_CAPTURE(f), ... back_args = FWD_CAPTURE(back_args)](auto &&...front_args) mutable
-           -> decltype(std::invoke(FWD(f), FWD(front_args)..., FWD(back_args)...)) {
-        return std::invoke(access(f), FWD(front_args)..., access(back_args)...);
+// clang-format off
+template<typename F>
+auto bind_back(F &&f, auto &&...back_args) {
+    return recursive{
+        overload{
+            [f = FWD_CAPTURE(f), ... back_args = FWD_CAPTURE(back_args)](auto &&self, auto &&...front_args) -> decltype(auto)
+                requires std::invocable<pigro::like_t<decltype(self), F>, decltype(front_args)..., decltype(back_args)...> {
+                return access(f)(FWD(front_args)..., access(back_args)...);
+            },
+            [f = FWD_CAPTURE(f), ... back_args = FWD_CAPTURE(back_args)](auto &&self, auto &&...front_args) mutable -> decltype(auto)
+                requires std::invocable<pigro::like_t<decltype(self), F>, decltype(front_args)..., decltype(back_args)...> {
+                return access(f)(FWD(front_args)..., access(back_args)...);
+            },
+        }
     };
 }
+// clang-format on
 
 auto bind_front_tuple(auto &&f, concepts::tuple_like auto &&t) {
     return recursive{
